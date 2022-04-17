2c6025ce8aa5c5e735bf517114de54be35a96e4b
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-17 13:51:36 -0500

    primer commit

---
+++ microservicio-cliente-cuenta-activa/src/main/java/nttdata/bootcamp/microservicios/cliente/cuenta/activa/MicroservicioClienteCuentaActivaApplication.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.cliente.cuenta.activa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioClienteCuentaActivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioClienteCuentaActivaApplication.class, args);
+	}
+
+}
---
+++ microservicio-cliente-cuenta-activa/src/test/java/nttdata/bootcamp/microservicios/cliente/cuenta/activa/MicroservicioClienteCuentaActivaApplicationTests.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.cliente.cuenta.activa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioClienteCuentaActivaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
