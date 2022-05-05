616fa4a474062189d67e7949c16a34039c804146
Joffre Hermosilla Salas <joffre.hermosilla@gmail.com>
2022-05-05 17:10:57 -0500

    kafka y redis

---
+++ microservicio-cliente-cuenta-activa/src/main/java/nttdata/bootcamp/microservicios/cliente/cuenta/activa/config/KafkaTopicConfig.java
@@ -0,0 +1,8 @@
+package nttdata.bootcamp.microservicios.cliente.cuenta.activa.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaTopicConfig {
+
+}
